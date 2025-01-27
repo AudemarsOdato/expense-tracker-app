749d06410a27038e897bec23e3c3572498ce25db
Audemars Odato <audemarsodato@gmail.com>
2025-01-27 14:53:20 +0800

    added transaction logic

--- script2.js
+++ script2.js
@@ -1,1 +1,29 @@
-// https://stackoverflow.com/questions/23108643/javascript-compare-variable-against-array-of-values
+const currentBalance_el = document.getElementById("current-balance");
+const amountInput = document.getElementById("amountInput");
+const categoryInput = document.getElementById("categoryOptions");
+const addTransactionButton = document.getElementById("addTransaction-button");
+const undoTransactionButton = document.getElementById("undoTransaction-button");
+const transactionLog = document.getElementById("log-list");
+
+var currentBalance = 0;
+
+const expenses = ["Food", "Transportation", "Academics", "Savings"];
+const incomes  = ["Received money", "Withdrew money"];
+
+addTransactionButton.onclick = () => {
+        
+        i
+};
+
+function determineTransactionType(category) {
+
+        if (expenses.includes(categoryInput.value)) {
+                return "expense";
+        }
+        else if (incomes.includes(categoryInput.value)) {
+                return "income";
+        }
+        else {
+                return "Invalid input!";
+        }
+}
